82133dbb6c044a4a04d9684ab131b350b6b19170
https://github.com/afi-syl/enyata-academy-2023.git <sylvialyril.sl@gmail.com>
2023-05-25 22:33:52 +0000

    adding 3 files

---
+++ 0x01-intro_to_css_and_html/responsiveness/index.js
@@ -0,0 +1,16 @@
+let myName = "Sylvia";
+You fisrt have to know which data you are dealing with , then you write "typeof" this will display the data type for myName
+myName = "Dugah";
+let car ={
+    name: "Toyota";
+    year: 2020;
+    color: "black";
+    model: "camry";
+    engine: "petrol";
+    transmission: "manual";
+    feature: "airbag";
+    weight: 170 + "km"
+    seatingCapacity: 4;
+    price: 30000 + "usd"
+
+}
---
+++ 0x01-intro_to_css_and_html/responsiveness/numbers.js
@@ -0,0 +1,8 @@
+const favoriteNumber= 2;
+const favoriteNumber= favoriteNumber / 2;
+const halfOfFavNum= 0;
+let isaacFavorite= 13;
+isaacFavorite= isaacFavorite - 2;
+isaacFavorite= 26 * 11;
+isaacFavorite= isaacFavorite / 3;
+console.log(isaacFavorite)
